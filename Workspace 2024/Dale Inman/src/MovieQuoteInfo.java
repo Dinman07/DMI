f246985b910ffc89f9fab40390cf57d96edc6243
Dinman07 <157329840+Dinman07@users.noreply.github.com>
2024-01-21 20:36:14 -0500

    Add files via upload

---
+++ Workspace 2024/Dale Inman/src/MovieQuoteInfo.java
@@ -0,0 +1,13 @@
+
+public class MovieQuoteInfo {
+
+	public static void main(String[] args) {
+
+		System.out.println("If you're not first, you're last.");
+		System.out.println("Talladega Nights");
+		System.out.println("Ricky Bobby");
+		System.out.println("2006");
+		
+	}
+
+}
---
+++ Workspace 2024/Dale Inman/src/SongLyrics.java
@@ -0,0 +1,13 @@
+
+public class SongLyrics {
+
+	public static void main(String[] args) {
+		System.out.println("A 40 HP Johnson on a flat bottom metal boat");
+		System.out.println("Coke cans and BB guns, barbed wire and old fence posts");
+		System.out.println("8-point bucks in Autumn and freshly cut corn fields");
+		System.out.println("One arm out the window and one hand on the wheel");
+		System.out.println("Some things just go better together and probably always will");
+
+	}
+
+}
